4a8ca183dffc7ba010091650552125bba0148cb6
Adam Charane <adamcharane@gmail.com>
2020-01-02 00:54:45 +0100

    Graph data structure

--- Graphs/Graph.cpp
+++ Graphs/Graph.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "Graph.h"
+
+#include <utility>
+
+Graph::Graph(int n) {
+    this->n_nodes = n;
+    this->graph.assign(n+1, set<int>());
+}
+
+Graph::Graph(int n, vector<edge> &edges) : Graph(n) {
+    for(auto edge : edges){
+        this->add_edge(edge);
+    }
+}
+
+void Graph::add_edge(edge e) {
+    this->edges.insert(e);
+    this->graph[e.first].insert(e.second);
+    this->graph[e.second].insert(e.first);
+}
+
+void Graph::remove_edge(edge e) {
+    this->edges.erase(e);
+    this->graph[e.first].erase(e.second);
+    this->graph[e.second].erase(e.first);
+}
+
+int Graph::number_edges() {
+    return this->edges.size();
+}
--- Graphs/Graph.h
+++ Graphs/Graph.h
@@ -5,9 +5,24 @@
 #ifndef CP_GRAPH_H
 #define CP_GRAPH_H
 
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> edge;
+
 
 class Graph {
 
+private:
+    int n_nodes;
+    set<edge> edges;
+    vector<set<int>> graph;
+public:
+    explicit Graph(int);
+    Graph(int n, vector<edge> &edges);
+    void add_edge(edge);
+    void remove_edge(edge);
+    int number_edges();
 };
 
 
