ab7037e14ec2dc8166e97472b9670e2f7a437d48
Adam Charane <adamcharane@gmail.com>
2020-01-02 00:53:10 +0100

    Graph data structure

---
+++ Graphs/Graph.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adam on 1/2/20.
+//
+
+#include "Graph.h"
---
+++ Graphs/Graph.h
@@ -0,0 +1,14 @@
+//
+// Created by adam on 1/2/20.
+//
+
+#ifndef CP_GRAPH_H
+#define CP_GRAPH_H
+
+
+class Graph {
+
+};
+
+
+#endif //CP_GRAPH_H
