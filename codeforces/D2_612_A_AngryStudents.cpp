36239f89e0b4ac8c1ada96568a4a5b69c30bba06
Adam Charane <adamcharane@gmail.com>
2020-01-06 16:54:40 +0100

    Codeforces Round 612 - Div2 - A

--- codeforces/D2_612_A_AngryStudents.cpp
+++ codeforces/D2_612_A_AngryStudents.cpp
@@ -1,4 +1,31 @@
-//
-// Created by adam on 1/6/20.
-//
+#include <bits/stdc++.h>
+using namespace std;
 
+// http://codeforces.com/contest/1287/problem/A
+
+int main(){
+    int groups;
+    cin >> groups;
+    for(int _=0; _<groups; _++){
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        int ans = 0, curr_ans=0;
+        bool a = false;
+        for(auto c : s){
+            if(c == 'P'){
+                if(a) curr_ans++;
+            }
+            else{
+                a = true;
+                ans = max(ans, curr_ans);
+                curr_ans = 0;
+            }
+        }
+        ans = max(ans, curr_ans);
+        cout << ans << endl;
+    }
+    return 0;
+}
